7f0d824b168ef85c09397f10b5760600a4cff3d5
Nicolas Evangelista <nicolasabreu87@gmail.com>
2025-11-04 09:29:32 -0300

    Tic Tac Toe Game is Coded!

--- TicTacToe/src/TicTacToe.java
+++ TicTacToe/src/TicTacToe.java
@@ -1,26 +1,50 @@
+import java.util.Scanner;
+
 public class TicTacToe {
     public static void main(String[] args) {
         char[] board = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
         var numberOfSquaresPlayed = 0;
-        var whoseTurnItIs= 'x';
-        while (numberOfSquaresPlayed < 9)
-        {
+        var whoseTurnItIs = 'x';
+        var scanner = new Scanner(System.in);
+
+        while (numberOfSquaresPlayed < 9) {
             printTheBoard(board);
-            System.out.printf("Choose a square player %s:", whoseTurnItIs);
+            System.out.printf("Escolha um espaço para jogar %s: ", whoseTurnItIs);
 
-            var scanner = new java.util.Scanner(System.in);
             var input = scanner.nextInt();
+            board[input - 1] = whoseTurnItIs;
 
-            board[input-1] = whoseTurnItIs;
+            if ((board[0] + board[1] + board[2] == whoseTurnItIs * 3)
+                    || (board[3] + board[4] + board[5] == whoseTurnItIs * 3)
+                    || (board[6] + board[7] + board[8] == whoseTurnItIs * 3)
+                    || (board[0] + board[3] + board[6] == whoseTurnItIs * 3)
+                    || (board[1] + board[4] + board[7] == whoseTurnItIs * 3)
+                    || (board[2] + board[5] + board[8] == whoseTurnItIs * 3)
+                    || (board[0] + board[4] + board[8] == whoseTurnItIs * 3)
+                    || (board[2] + board[4] + board[6] == whoseTurnItIs * 3)) {
+
+                printTheBoard(board);
+                System.out.println("Voce Ganhou! Parabéns!");
+                break;
+            } else {
+                numberOfSquaresPlayed++;
+                whoseTurnItIs = (whoseTurnItIs == 'x') ? 'o' : 'x';
+            }
+
+            if (numberOfSquaresPlayed == 9) {
+                printTheBoard(board);
+                System.out.println("Deu Empate!");
+            }
         }
-    }
 
-    private static void printTheBoard(char[] board) { // Imprime a tabela
-        System.out.println( board[0] + " | " +  board[1] + " | " + board[2]);
-        System.out.println( " - + - + - " );
-        System.out.println( board[3] + " | " +  board[4] + " | " + board[5]);
-        System.out.println( " - + - + - " );
-        System.out.println( board[6] + " | " +  board[7] + " | " + board[8]);
-    }
-}
-// Step 4
+        scanner.close(); // <- fecha scanner
+    } // <- fecha main
+
+    private static void printTheBoard(char[] board) {
+        System.out.println("" + board[0] + " | " + board[1] + " | " + board[2]);
+        System.out.println(" - + - + - ");
+        System.out.println("" + board[3] + " | " + board[4] + " | " + board[5]);
+        System.out.println(" - + - + - ");
+        System.out.println("" + board[6] + " | " + board[7] + " | " + board[8]);
+    } // <- fecha printTheBoard
+} // <- fecha classe TicTacToe
